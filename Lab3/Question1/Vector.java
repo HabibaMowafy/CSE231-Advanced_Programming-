f20454f692f059372961e57eee94706ace0c82a2
HabibaMowafy <154655475+Lucifer3224@users.noreply.github.com>
2024-07-04 13:49:26 +0300

    Update Vector.java

--- Lab3/Question1/Vector.java
+++ Lab3/Question1/Vector.java
@@ -1,9 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Question1;
-
 import static java.lang.Math.*;
 import java.util.Scanner;
 
